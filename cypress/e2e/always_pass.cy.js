1cc79fadb6078d0d41a9946a912bcd1582bcb884
bluengo <bluengopriet@gmail.com>
2023-02-17 19:15:38 +0100

    Moved files to subfolder

--- cypress/e2e/always_pass.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Dummy PASS test to check the execution', () => {
-  it('Just passes!', () => {
-    expect(true).to.equal(true)
-  })
-})
-
--- cypress/e2e/basic_test_dynamic-plugin.cy.js
+++
@@ -1,5 +0,0 @@
-describe('template spec', () => {
-  it('Visits the Environments page to check Dynamic Plugin', () => {
-    cy.visit(Cypress.env('OCP_CONSOLE') + '/environments')
-  })
-})
